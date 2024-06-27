722da6e129a1a2699afff9c9136de552c2f147ac
martastarnawska <marta.chelm@gmail.com>
2024-06-27 13:34:19 -0400

    style Hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -15,13 +15,12 @@ const HeroSection = () => {
       </div>
       <h1 className="hero__header">
         Our daily bread is to connect right people in Poland and America in their desire to create and tighten bonds in business, culture or simply in 
-        <b> bringing communities together </b>
+        <b className="hero__header--emphasize"> bringing communities together </b>
       </h1>
       <p className="hero__description">
-        <b>Lorem ipsum dolor sit amet, </b>
+        <b className="hero__description--emphasize ">Lorem ipsum dolor sit amet, </b>
         consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et. 
       </p>
-      
     </section>
   )
 };
