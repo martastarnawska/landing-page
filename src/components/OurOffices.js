840010f7b25054e6afff745b5ba53095de7840d3
martastarnawska <marta.chelm@gmail.com>
2024-07-16 13:10:14 -0400

    create and style Our Offices section

---
+++ src/components/OurOffices.js
@@ -0,0 +1,31 @@
+import './OurOffices.scss';
+
+const OurOffices = () => (
+  <section className="offices">
+    <div className="offices__heading">
+      <span className="dot offices__dot"></span>
+      <h2 className="offices__header">Our Offices</h2>
+    </div>
+    <div className="offices__content">
+      <div className="offices__us">
+        <p>United States</p>
+        <p>Empire State Building</p>
+        <p>New York</p>
+      </div>
+      <div className='divider'>
+        <p className='dot divider__dot'></p>
+        <p className='dot dot--gray divider__dot'></p>
+        <p className='dot dot--gray divider__dot'></p>
+        <p className='dot dot--gray divider__dot'></p>
+        <p className='dot dot--gray divider__dot'></p>
+      </div>
+      <div className="offices__pl">
+        <p>Poland</p>
+        <p>Warsaw Spire </p>
+        <p>Warsaw</p>
+      </div>
+    </div>
+  </section>
+);
+
+export default OurOffices;
