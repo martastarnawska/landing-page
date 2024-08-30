cf881605b8e615093c9169f5902399796c17f034
martastarnawska <marta.chelm@gmail.com>
2024-08-30 14:56:05 -0400

    first subpage example

---
+++ src/pages/GeneralConsulting.js
@@ -0,0 +1,12 @@
+import Subpage from "@/components/Subpage";
+
+const title = "General Consulting";
+const textContent = "Our daily bread is to connect right people in Poland and America in their desire to create and tighten bonds in business, culture or simply in bringing communities together. Our daily bread is to connect right people in Poland and America in their desire to create and tighten bonds in business, culture or simply in bringing communities together. Our daily bread is to connect right people in Poland and America in their desire to create and tighten bonds in business, culture or simply in bringing communities together. Our daily bread is to connect right people in Poland and America in their desire to create and tighten bonds in business, culture or simply in bringing communities together.Our daily bread is to connect right people in Poland and America in their desire to create and tighten bonds in business, culture or simply in bringing communities together";
+
+const GeneralConsulting = () => (
+  <Subpage title={title}>
+    <p>{textContent}</p>
+  </Subpage>
+); 
+
+export default GeneralConsulting;
