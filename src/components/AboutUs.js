90c90f8fa0ee9290c07b9a5847ae4101d771cc92
martastarnawska <marta.chelm@gmail.com>
2024-10-09 12:57:53 -0400

    placeholder for name and surname

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -22,9 +22,9 @@ const AboutUs = () => {
           <p className="card__name">
             <span className="card__firstName">
               <span className="dot card__dot"></span>
-              <span>Imię</span>
+              <span>Name</span>
             </span>
-            <b className="card__lastName">Nazwisko</b>
+            <b className="card__lastName">Surname</b>
           </p>
           <p className="card__bio">
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et 
@@ -40,10 +40,10 @@ const AboutUs = () => {
         <div className="card__description  card__description--second">
           <p className="card__name">
           <span className="card__firstName">
-              <span>Imię</span>
+              <span>Name</span>
               <span className="dot card__dot"></span>
             </span>
-            <b className="card__lastName">Nazwisko</b>
+            <b className="card__lastName">Surname</b>
           </p>
           <p className="card__bio">
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et 
