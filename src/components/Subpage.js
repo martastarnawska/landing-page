afa8f57423ddb93a308b14ce11d50117cc8ee6e2
martastarnawska <marta.chelm@gmail.com>
2024-10-08 22:08:00 -0400

    use Document to load stylesheet for subpages

--- src/components/Subpage.js
+++ src/components/Subpage.js
@@ -1,24 +1,18 @@
-import Head from "next/head";
 import Navigation from "@/components/Navigation";
 import "./Subpage.scss";
 
 const Subpage = ({ title, children}) => (
-  <>
-    <Head>
-      <link rel="stylesheet" href="https://use.typekit.net/tef7uvt.css"/> 
-    </Head>
-    <section className="subpage">
-      <div className="subpage__box">
-        <Navigation />
-        <div className="subpage__contentWrapper">
-          <h1 className="subpage__title">{title}</h1>
-          <div className="subpage__content">
-            {children}
-          </div>
+  <section className="subpage">
+    <div className="subpage__box">
+      <Navigation />
+      <div className="subpage__contentWrapper">
+        <h1 className="subpage__title">{title}</h1>
+        <div className="subpage__content">
+          {children}
         </div>
       </div>
-    </section>
-  </>
+    </div>
+  </section>
 );
 
 export default Subpage;
