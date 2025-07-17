6fd54fff0a3ef7dd16eea059718073a3d2575300
martastarnawska <marta.chelm@gmail.com>
2025-07-17 15:57:21 +0200

    adjust Hero component with updated texts and translations

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -34,15 +34,16 @@ const HeroSection = () => {
         </div>
         <h1 className="hero__header">
           <Trans i18nKey="hero.welcomeText" t={t}>
-            initial description
-            <b className="hero__header--emphasize"> emphasize fraze </b>
+            Our daily bread is to
+            <b className="hero__header--emphasize"> connect right people in Poland and America </b>
+            in their desire to create and tighten bonds in business, culture or simply in bringing communities together.
           </Trans>
         </h1>
       </div>
-      <p className="hero__description">
-        <b className="hero__description--emphasize ">Lorem ipsum dolor sit amet, </b>
-        consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et. 
-      </p>
+      <div className="hero__description">
+        <p>{t('hero.description-part1')}</p>
+        <p>{t('hero.description-part2')}</p>
+      </div>
     </section>
   )
 };
